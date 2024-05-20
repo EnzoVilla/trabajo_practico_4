a1b5fdf6e825e151a64ec2cd422f15d771156b23
EnzoVilla <enzovilla1810@gmail.com>
2024-05-20 20:35:16 -0300

    paquetes iniciales del tp4

---
+++ tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/Tp4VillagranEnzoMauricioApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4VillagranEnzoMauricioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4VillagranEnzoMauricioApplication.class, args);
+	}
+
+}
