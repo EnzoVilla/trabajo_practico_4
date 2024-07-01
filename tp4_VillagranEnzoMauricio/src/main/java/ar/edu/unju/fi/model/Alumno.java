23c59c271a014465ef193d14308f081736caff20
EzequielVillalba10 <ezequielvillagf@gmail.com>
2024-06-30 21:13:05 -0300

    Agrego notacion de lombook a la clase model Alumno y Docente

--- tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/model/Alumno.java
+++ tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/model/Alumno.java
@@ -5,7 +5,14 @@ import java.time.LocalDate;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Component;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Alumno {
 	private String dni;
 	private String nombre;
@@ -16,73 +23,5 @@ public class Alumno {
 	private LocalDate fechaNacimiento;
 	private String domicilio;
 	private int lu;
-	public Alumno() {
-		// TODO Auto-generated constructor stub
-	}
-	public Alumno(String dni, String nombre, String apellido, String email, long telefono, LocalDate fechaNacimiento,
-			String domicilio, int lu) {
-		this.dni = dni;
-		this.nombre = nombre;
-		this.apellido = apellido;
-		this.email = email;
-		this.telefono = telefono;
-		this.fechaNacimiento = fechaNacimiento;
-		this.domicilio = domicilio;
-		this.lu = lu;
-	}
-	public String getDni() {
-		return dni;
-	}
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public String getApellido() {
-		return apellido;
-	}
-	public void setApellido(String apellido) {
-		this.apellido = apellido;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public long getTelefono() {
-		return telefono;
-	}
-	public void setTelefono(long telefono) {
-		this.telefono = telefono;
-	}
-	public LocalDate getFechaNacimiento() {
-		return fechaNacimiento;
-	}
-	public void setFechaNacimiento(LocalDate fechaNacimiento) {
-		this.fechaNacimiento = fechaNacimiento;
-	}
-	public String getDomicilio() {
-		return domicilio;
-	}
-	public void setDomicilio(String domicilio) {
-		this.domicilio = domicilio;
-	}
-	public int getLu() {
-		return lu;
-	}
-	public void setLu(int lu) {
-		this.lu = lu;
-	}
-	@Override
-	public String toString() {
-		return "Alumno [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email
-				+ ", telefono=" + telefono + ", fechaNacimiento=" + fechaNacimiento + ", domicilio=" + domicilio
-				+ ", lu=" + lu + "]";
-	}
 	
 }
