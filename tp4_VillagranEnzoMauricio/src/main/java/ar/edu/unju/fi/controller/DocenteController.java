bad95b7b259a1d4912b021cb3aae63420edea63f
EzequielVillalba10 <ezequielvillagf@gmail.com>
2024-07-01 02:18:44 -0300

    Modifico el controlador Docente!

--- tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
+++ tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
@@ -9,14 +9,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import ar.edu.unju.fi.collections.CollectionDocente;
-import ar.edu.unju.fi.model.Docente;
+import ar.edu.unju.fi.dto.DocenteDTO;
+import ar.edu.unju.fi.service.IDocenteService;
 
 @Controller
 @RequestMapping("/docente")
 public class DocenteController {
 	@Autowired
-	private Docente docente;
+	private DocenteDTO docenteDTO; 
+	
+	@Autowired
+	private IDocenteService docenteService;
 	
 	@GetMapping("/listado")
 	public String getDocentesPage(Model model) {
@@ -24,7 +27,7 @@ public class DocenteController {
 		String mensaje="";
 		model.addAttribute("exito", exito);
 		model.addAttribute("mensaje", mensaje);
-		model.addAttribute("docentes", CollectionDocente.getDocente());
+		model.addAttribute("docentes", docenteService.findALL());
 		model.addAttribute("titulo", "Docentes");
 		return("docentes");
 	}
@@ -32,14 +35,14 @@ public class DocenteController {
 	public String getNuevoDocente(Model model) {
 		boolean edicion=false;
 		model.addAttribute("edicion", edicion);
-		model.addAttribute("docente", docente);
+		model.addAttribute("docente", docenteDTO);
 		model.addAttribute("titulo", "Nuevo Docente");
 		return("docente");
 	}
 	@PostMapping("/guardar")
-	public ModelAndView guardar(@ModelAttribute("docente") Docente docente) {
+	public ModelAndView guardar(@ModelAttribute("docente") DocenteDTO docenteDTO) {
 		ModelAndView modelAndView = new ModelAndView("docentes");
-		boolean exito = CollectionDocente.agregarDocente(docente);
+		boolean exito = docenteService.save(docenteDTO);
 		String mensaje;
 		if(exito) {
 			mensaje="Se guardo docente con exito";
@@ -48,39 +51,39 @@ public class DocenteController {
 		}
 		modelAndView.addObject("exito", exito);
 		modelAndView.addObject("mensaje", mensaje);
-		modelAndView.addObject("docentes", CollectionDocente.getDocente());
+		modelAndView.addObject("docentes", docenteService.findALL());
 		return modelAndView;
 	}
 	@GetMapping("/modificar/{legajo}")
 	public String getModificarDocente(Model model, @PathVariable(value="legajo") int legajo) {
 		boolean edicion = true;
-		Docente docenteEncontrado = new Docente();
-		docenteEncontrado = CollectionDocente.buscarDocente(legajo);
-		model.addAttribute("docente", docenteEncontrado);
+		DocenteDTO docenteEncontradoDTO = new DocenteDTO();
+		docenteEncontradoDTO = docenteService.findByLegajo(legajo);
+		model.addAttribute("docente", docenteEncontradoDTO);
 		model.addAttribute("edicion", edicion);
 		model.addAttribute("titulo", "Modificar docente");
 		return("docente");
 	}
 	@PostMapping("/modificar")
-	public String modificarDocente(@ModelAttribute("docente") Docente docente, Model model) {
+	public String modificarDocente(@ModelAttribute("docente") DocenteDTO docenteDTO, Model model) {
 		boolean exito=false;
 		String mensaje="";
 		try {
-			CollectionDocente.modificarDocente(docente);
-			mensaje="El docente con legajo "+docente.getLegajo()+" se modifico con exito";
+			docenteService.edit(docenteDTO);
+			mensaje="El docente con legajo "+docenteDTO.getLegajo()+" se modifico con exito";
 			exito=true;
 		}catch(Exception e) {
 			mensaje=e.getMessage();
 		}
 		model.addAttribute("exito", exito);
 		model.addAttribute("mensaje", mensaje);
-		model.addAttribute("docentes", CollectionDocente.getDocente());
+		model.addAttribute("docentes", docenteService.findALL());
 		model.addAttribute("titulo", "Docentes");
 		return("docentes");
 	}
 	@GetMapping("/eliminar/{legajo}")
 	public String eliminarDocente(@PathVariable(value="legajo") int legajo) {
-		CollectionDocente.eliminarDocente(legajo);
+		docenteService.delateBylegajo(legajo);
 		return("redirect:/docente/listado");
 	}
 }
