8bfd8754ae9e43594b45982c13805864fdee8e64
EzequielVillalba10 <ezequielvillagf@gmail.com>
2024-06-30 21:31:57 -0300

    Agrego clases en el paquete DTO: AlumnoDTO y DocenteDTO

---
+++ tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/dto/DocenteDTO.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.dto;
+
+import org.springframework.stereotype.Component;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Component
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocenteDTO {
+	private int legajo;
+	private String nombre;
+	private String apellido;
+	private String email;
+	private long telefono;
+}
