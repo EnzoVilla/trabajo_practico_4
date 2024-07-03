7f676901a2b06008b812fea074f07401ee2d4e5e
EnzoVilla <enzovilla1810@gmail.com>
2024-07-03 03:02:12 -0300

    Desarrollo de repositorios de carrera y materia Close #33

--- tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/model/Carrera.java
+++ tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/model/Carrera.java
@@ -2,16 +2,23 @@ package ar.edu.unju.fi.model;
 
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Component
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@Entity
+@Table(name="carrera")
 public class Carrera {
-	private int codigo;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer codigo;
 	private String nombre;
 	private int cantAnios;
 	private boolean estado;
