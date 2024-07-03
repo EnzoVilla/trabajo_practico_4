4d5547114079a63a364aceb71e9dabc1ccd5e2c4
EzequielVillalba10 <ezequielvillagf@gmail.com>
2024-07-03 03:08:40 -0300

    Dearrollo de repositorio para alumno y docente!

--- tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/model/Docente.java
+++ tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/model/Docente.java
@@ -3,15 +3,21 @@ package ar.edu.unju.fi.model;
 import org.springframework.stereotype.Component;
 
 import jakarta.persistence.Entity;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "docentes")
 @Component
 @Data
 @NoArgsConstructor
 public class Docente {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer legajo;
 	private String nombre;
 	private String apellido;
