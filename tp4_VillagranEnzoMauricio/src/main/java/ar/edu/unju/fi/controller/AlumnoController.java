a1d2a146895efb63ebcd3e3f63a15067407f7a3a
EzequielVillalba10 <ezequielvillagf@gmail.com>
2024-07-08 04:15:31 -0300

    Modifico los controladores!

--- tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
+++ tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/controller/AlumnoController.java
@@ -3,6 +3,7 @@ package ar.edu.unju.fi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,85 +12,102 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import ar.edu.unju.fi.dto.AlumnoDTO;
 import ar.edu.unju.fi.service.IAlumnoService;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/alumno")
 public class AlumnoController {
-	@Autowired
-	private AlumnoDTO alumnoDTO;
-	
-	@Autowired
-	private IAlumnoService alumnoService;
-	
-	@GetMapping("/listado")
-	public String getAlumnosPage(Model model) {
-		boolean exito=false;
-		String mensaje="";
-		model.addAttribute("exito", exito);
-		model.addAttribute("mensaje", mensaje);
-		model.addAttribute("alumnos", alumnoService.findALL());
-		model.addAttribute("titulo", "Alumnos");
-		return("alumnos");
-	}
-	@GetMapping("/nueva")
-	public String getNuevoAlumno(Model model) {
-		boolean edicion = false;
-		model.addAttribute("alumno", alumnoDTO);
-		model.addAttribute("edicion", edicion);
-		model.addAttribute("titulo", "Nuevo alumno");
-		return("alumno");
-	}
-	@PostMapping("/guardar")
-	public ModelAndView guardar(@ModelAttribute("alumno") AlumnoDTO alumnoDTO) {
-		ModelAndView modelAndView = new ModelAndView("alumnos");
-		String mensaje;
-		boolean exito = true;
-		alumnoDTO.setEstado(true);
-		alumnoService.save(alumnoDTO);
-		if(exito) {
-			mensaje="Se guardo el alumno con exito";
-		}else {
-			mensaje="No se pudo guardar";
-		}
-		modelAndView.addObject("exito", exito);
-		modelAndView.addObject("mensaje", mensaje);
-		modelAndView.addObject("alumnos", alumnoService.findALL());
-		return modelAndView;
-	}
-	@GetMapping("/modificar/{lu}")
-	public String getModificarAlumno(Model model, @PathVariable(value="lu") int lu) {
-		boolean edicion = true;
-		AlumnoDTO alumnoEncontradoDTO = new AlumnoDTO();
-		alumnoEncontradoDTO = alumnoService.findByLu(lu);
-		alumnoEncontradoDTO.getFechaNacimiento();
-		model.addAttribute("edicion", edicion);
-		model.addAttribute("alumno", alumnoEncontradoDTO);
-		model.addAttribute("titulo", "Modificar alumno");
-		return("alumno");
-	}
-	@PostMapping("/modificar")
-	public String modificarAlumno(@ModelAttribute("alumno") AlumnoDTO alumnoDTO, Model model) {
-		boolean exito=false;
-		String mensaje="";
-		alumnoDTO.setEstado(true);
-		try {
-			alumnoService.edit(alumnoDTO);
-			mensaje="El alumno con libreta "+alumnoDTO.getLu()+" fue modificada con exito";
-			exito=true;
-		}catch(Exception e){
-			mensaje=e.getMessage();
-			e.printStackTrace();
-		}
-		model.addAttribute("mensaje", mensaje);
-		model.addAttribute("exito", exito);
-		model.addAttribute("alumnos", alumnoService.findALL());
-		model.addAttribute("titulo", "Alumnos");
-		return("alumnos");
-	}
-	@GetMapping("/eliminar/{lu}")
-	public String eliminarCarrera(@PathVariable(value="lu") int lu) {
-		alumnoService.delateByLu(lu);
-		return("redirect:/alumno/listado");
-	}
-	
+
+    @Autowired
+    private AlumnoDTO alumnoDTO;
+    
+    @Autowired
+    private IAlumnoService alumnoService;
+    
+    @GetMapping("/listado")
+    public String getAlumnosPage(Model model) {
+        boolean exito=false;
+        String mensaje="";
+        model.addAttribute("exito", exito);
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("alumnos", alumnoService.findALL());
+        model.addAttribute("titulo", "Alumnos");
+        return "alumnos";
+    }
+    
+    @GetMapping("/nueva")
+    public String getNuevoAlumno(Model model) {
+        boolean edicion = false;
+        model.addAttribute("alumno", alumnoDTO);
+        model.addAttribute("edicion", edicion);
+        model.addAttribute("titulo", "Nuevo alumno");
+        return "alumno";
+    }
+    
+    @PostMapping("/guardar")
+    public ModelAndView guardar(@Valid @ModelAttribute("alumno") AlumnoDTO alumnoDTO, BindingResult result) {
+        ModelAndView modelAndView = new ModelAndView("alumnos");
+        if (result.hasErrors()) {
+            modelAndView.setViewName("alumno");
+            modelAndView.addObject("edicion", false);
+            modelAndView.addObject("titulo", "Nuevo Alumno");
+            return modelAndView;
+        }
+        
+        String mensaje;
+        boolean exito = true;
+        alumnoDTO.setEstado(true);
+        alumnoService.save(alumnoDTO);
+        if (exito) {
+            mensaje = "Se guardó el alumno con éxito";
+        } else {
+            mensaje = "No se pudo guardar";
+        }
+        modelAndView.addObject("exito", exito);
+        modelAndView.addObject("mensaje", mensaje);
+        modelAndView.addObject("alumnos", alumnoService.findALL());
+        return modelAndView;
+    }
+    
+    @GetMapping("/modificar/{lu}")
+    public String getModificarAlumno(Model model, @PathVariable(value = "lu") int lu) {
+        boolean edicion = true;
+        AlumnoDTO alumnoEncontradoDTO = alumnoService.findByLu(lu);
+        model.addAttribute("edicion", edicion);
+        model.addAttribute("alumno", alumnoEncontradoDTO);
+        model.addAttribute("titulo", "Modificar alumno");
+        return "alumno";
+    }
+    
+    @PostMapping("/modificar")
+    public String modificarAlumno(@Valid @ModelAttribute("alumno") AlumnoDTO alumnoDTO, BindingResult result, Model model) {
+    	 if (result.hasErrors()) {
+    	        model.addAttribute("edicion", true);
+    	        model.addAttribute("titulo", "Modificar Alumno");
+    	        return "alumno";
+    	    }
+        
+        boolean exito = false;
+        String mensaje = "";
+        alumnoDTO.setEstado(true);
+        try {
+            alumnoService.edit(alumnoDTO);
+            mensaje = "El alumno con libreta " + alumnoDTO.getLu() + " fue modificado con éxito";
+            exito = true;
+        } catch (Exception e) {
+            mensaje = e.getMessage();
+            e.printStackTrace();
+        }
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("exito", exito);
+        model.addAttribute("alumnos", alumnoService.findALL());
+        model.addAttribute("titulo", "Alumnos");
+        return "alumnos";
+    }
+    
+    @GetMapping("/eliminar/{lu}")
+    public String eliminarCarrera(@PathVariable(value = "lu") int lu) {
+        alumnoService.delateByLu(lu);
+        return "redirect:/alumno/listado";
+    }
 }
--- tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
+++ tp4_VillagranEnzoMauricio/src/main/java/ar/edu/unju/fi/controller/DocenteController.java
@@ -3,93 +3,99 @@ package ar.edu.unju.fi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
 import ar.edu.unju.fi.dto.DocenteDTO;
 import ar.edu.unju.fi.service.IDocenteService;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/docente")
 public class DocenteController {
-	@Autowired
-	private DocenteDTO docenteDTO; 
-	
-	@Autowired
-	private IDocenteService docenteService;
-	
-	@GetMapping("/listado")
-	public String getDocentesPage(Model model) {
-		boolean exito=false;
-		String mensaje="";
-		model.addAttribute("exito", exito);
-		model.addAttribute("mensaje", mensaje);
-		model.addAttribute("docentes", docenteService.findALL());
-		model.addAttribute("titulo", "Docentes");
-		return("docentes");
-	}
-	@GetMapping("/nueva")
-	public String getNuevoDocente(Model model) {
-		boolean edicion=false;
-		model.addAttribute("edicion", edicion);
-		model.addAttribute("docente", docenteDTO);
-		model.addAttribute("titulo", "Nuevo Docente");
-		return("docente");
-	}
-	@PostMapping("/guardar")
-	public ModelAndView guardar(@ModelAttribute("docente") DocenteDTO docenteDTO) {
-		ModelAndView modelAndView = new ModelAndView("docentes");
-		boolean exito = true; 
-		docenteDTO.setEstado(true);
-		docenteService.save(docenteDTO);
-		String mensaje;
-		if(exito) {
-			mensaje="Se guardo docente con exito";
-		}else {
-			mensaje="No se pudo guardar";
-		}
-		modelAndView.addObject("exito", exito);
-		modelAndView.addObject("mensaje", mensaje);
-		modelAndView.addObject("docentes", docenteService.findALL());
-		return modelAndView;
-	}
-	@GetMapping("/modificar/{legajo}")
-	public String getModificarDocente(Model model, @PathVariable(value="legajo") int legajo) {
-		boolean edicion = true;
-		DocenteDTO docenteEncontradoDTO = new DocenteDTO();
-		docenteEncontradoDTO = docenteService.findByLegajo(legajo);
-		model.addAttribute("docente", docenteEncontradoDTO);
-		model.addAttribute("edicion", edicion);
-		model.addAttribute("titulo", "Modificar docente");
-		return("docente");
-		
-		
-	}
-	@PostMapping("/modificar")
-	public String modificarDocente(@ModelAttribute("docente") DocenteDTO docenteDTO, Model model) {
-		boolean exito=false;
-		String mensaje="";
-		docenteDTO.setEstado(true);
-		try {
-			docenteService.edit(docenteDTO);
-			mensaje="El docente con legajo "+docenteDTO.getLegajo()+" se modifico con exito";
-			exito=true;
-		}catch(Exception e) {
-			mensaje=e.getMessage();
-		}
-		model.addAttribute("exito", exito);
-		model.addAttribute("mensaje", mensaje);
-		model.addAttribute("docentes", docenteService.findALL());
-		model.addAttribute("titulo", "Docentes");
-		return("docentes");
-		
-	}
-	@GetMapping("/eliminar/{legajo}")
-	public String eliminarDocente(@PathVariable(value="legajo") int legajo) {
-		docenteService.delateBylegajo(legajo);
-		return("redirect:/docente/listado");
-	}
+    @Autowired
+    private DocenteDTO docenteDTO;
+
+    @Autowired
+    private IDocenteService docenteService;
+
+    @GetMapping("/listado")
+    public String getDocentesPage(Model model) {
+        model.addAttribute("exito", false);
+        model.addAttribute("mensaje", "");
+        model.addAttribute("docentes", docenteService.findALL());
+        model.addAttribute("titulo", "Docentes");
+        return "docentes";
+    }
+
+    @GetMapping("/nueva")
+    public String getNuevoDocente(Model model) {
+        model.addAttribute("edicion", false);
+        model.addAttribute("docente", docenteDTO);
+        model.addAttribute("titulo", "Nuevo Docente");
+        return "docente";
+    }
+
+    @PostMapping("/guardar")
+    public ModelAndView guardar(@Valid @ModelAttribute("docente") DocenteDTO docenteDTO, BindingResult result) {
+        if (result.hasErrors()) {
+            ModelAndView modelAndView = new ModelAndView("docente");
+            modelAndView.addObject("edicion", false);
+            modelAndView.addObject("docente", docenteDTO);
+            modelAndView.addObject("titulo", "Nuevo Docente");
+            return modelAndView;
+        }
+
+        docenteDTO.setEstado(true);
+        docenteService.save(docenteDTO);
+        ModelAndView modelAndView = new ModelAndView("redirect:/docente/listado");
+        modelAndView.addObject("exito", true);
+        modelAndView.addObject("mensaje", "Se guardó docente con éxito");
+        return modelAndView;
+    }
+
+    @GetMapping("/modificar/{legajo}")
+    public String getModificarDocente(Model model, @PathVariable(value = "legajo") int legajo) {
+        DocenteDTO docenteEncontradoDTO = docenteService.findByLegajo(legajo);
+        model.addAttribute("docente", docenteEncontradoDTO);
+        model.addAttribute("edicion", true);
+        model.addAttribute("titulo", "Modificar Docente");
+        return "docente";
+    }
+
+    @PostMapping("/modificar")
+    public String modificarDocente(@Valid @ModelAttribute("docente") DocenteDTO docenteDTO, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("docente", docenteDTO);
+            model.addAttribute("edicion", true);
+            model.addAttribute("titulo", "Modificar Docente");
+            return "docente";
+        }
+
+        docenteDTO.setEstado(true);
+        try {
+            docenteService.edit(docenteDTO);
+            model.addAttribute("exito", true);
+            model.addAttribute("mensaje", "El docente con legajo " + docenteDTO.getLegajo() + " se modificó con éxito");
+        } catch (Exception e) {
+            model.addAttribute("exito", false);
+            model.addAttribute("mensaje", "Error al modificar el docente: " + e.getMessage());
+        }
+
+        model.addAttribute("docentes", docenteService.findALL());
+        model.addAttribute("titulo", "Docentes");
+        return "docentes";
+    }
+
+    @GetMapping("/eliminar/{legajo}")
+    public String eliminarDocente(@PathVariable(value = "legajo") int legajo) {
+        docenteService.delateBylegajo(legajo);
+        return "redirect:/docente/listado";
+    }
 }
